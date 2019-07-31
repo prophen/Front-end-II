947d21444002cb935af86edc53ec521a2c16bb30
Nikema Prophet <nikema@gmail.com>
2019-07-31 16:16:59 -0700

    Add links to Nav

--- a-to-z/a-to-z/src/containers/NavBar/NavBar.js
+++ a-to-z/a-to-z/src/containers/NavBar/NavBar.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import { Menu, Image } from 'semantic-ui-react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Menu, Image } from "semantic-ui-react";
+import AddExperience from "../AddExperience/AddExperience";
+import SignUpForm from "../SignUpForm/SignUpForm";
+import LoginForm from "../LoginForm/LoginForm";
 
-const NavBar = () =>{
-    return(
-        <div>
-            <Menu>
-            {/* logo */}
-            <Image src="https://i.ibb.co/kc2Lq8X/atoz.png" size='tiny' width='200px'/>
+const NavBar = () => {
+  return (
+    <div>
+      <Menu>
+        {/* logo */}
+        <Image
+          src="https://i.ibb.co/kc2Lq8X/atoz.png"
+          size="tiny"
+          width="200px"
+        />
 
-            {/* tabs */}
-            <Menu.Menu className='menu-tabs' position='right'>
-                <Menu.Item name='Host an experience'/>
-                <Menu.Item name='Sign up'/>
-                <Menu.Item name='Log in'/>
-            </Menu.Menu>
-            </Menu>
-
-        </div>
-
-    )
-}
+        {/* tabs */}
+        <Menu.Menu className="menu-tabs" position="right">
+          <NavLink to="/experiences/add">
+            {" "}
+            <Menu.Item name="Host an experience" />
+          </NavLink>
+          <NavLink to="/signup">
+            {" "}
+            <Menu.Item name="Sign up" />
+          </NavLink>
+          <NavLink to="/login">
+            <Menu.Item name="Log in" />
+          </NavLink>
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
+};
 
 export default NavBar;
