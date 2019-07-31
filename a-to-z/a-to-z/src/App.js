73da313edd84b4a20712190fbb47ba594327ac20
Red <vhanchar@ucsc.edu>
2019-07-31 08:45:18 -0700

    added signup and login forms

--- a-to-z/a-to-z/src/App.js
+++ a-to-z/a-to-z/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import NavBar from './NavBar'
-import ExperienceTypeList from './ExperienceTypeList'
-import AllExperiences from './AllExperiences'
+import NavBar from './NavBar';
+import ExperienceTypeList from './ExperienceTypeList';
+import AllExperiences from './AllExperiences';
+import LoginForm from './LoginForm';
+import SignUp from './SignUp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ExperienceTypeList/>
-      <AllExperiences/>
+      {/* <ExperienceTypeList/>
+      <AllExperiences/> */}
+
+      {/* what values will we have, where are we making axios to? */}
+      
+      <LoginForm/>
+      <SignUp/>
+
     </div>
   );
 }
