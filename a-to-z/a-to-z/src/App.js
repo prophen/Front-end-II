0626d95e93252dc4784c41872113b426d2e15680
Nikema Prophet <nikema@gmail.com>
2019-07-30 18:39:23 -0700

    Move home components to AppRouter.js

--- a-to-z/a-to-z/src/App.js
+++ a-to-z/a-to-z/src/App.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import AppRouter from './AppRouter'
-import NavBar from './NavBar'
-import ExperienceTypeList from './ExperienceTypeList'
-import AllExperiences from './AllExperiences'
+import React from "react";
+import AppRouter from "./AppRouter";
+import NavBar from "./NavBar";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <ExperienceTypeList/>
-      <AllExperiences />
+      <NavBar />
       <AppRouter />
-
     </div>
   );
 }
--- a-to-z/a-to-z/src/AppRouter.js
+++ a-to-z/a-to-z/src/AppRouter.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AddExperience from "./containers/AddExperience/AddExperience";
-import SingleExperience from "./containers/SingleExperience/SingleExperience"
-import App from "./App"
+import SingleExperience from "./containers/SingleExperience/SingleExperience";
+import ExperienceTypeList from "./ExperienceTypeList";
+import AllExperiences from "./AllExperiences";
 
 export default function AppRouter() {
   return (
-    <Switch>
-      <Route path="/experiences/add" component={AddExperience} />
-      <Route path="/experiences/:id" component={SingleExperience} />
-    </Switch>
+    <>
+      <Route exact path="/" component={ExperienceTypeList} />
+      <Route exact path="/" component={AllExperiences} />
+
+      <Switch>
+        <Route path="/experiences/add" component={AddExperience} />
+        <Route path="/experiences/:id" component={SingleExperience} />
+      </Switch>
+    </>
   );
 }
