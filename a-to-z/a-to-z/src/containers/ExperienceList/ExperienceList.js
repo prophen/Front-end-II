4d0b51768eb440d55227ae65424312a2dc92694f
Nikema Prophet <nikema@gmail.com>
2019-07-31 13:33:32 -0700

    Create ExperienceCard component

--- a-to-z/a-to-z/src/containers/ExperienceList/ExperienceList.js
+++ a-to-z/a-to-z/src/containers/ExperienceList/ExperienceList.js
@@ -1,33 +1,18 @@
-import React from "react";
-import { experiences } from "../../data/experiences";
-import {
-  Card,
-  Image,
-} from "semantic-ui-react";
+import React, {useState} from "react";
+import { experiences as experienceData } from "../../data/experiences";
+
+import ExperienceCard from "./ExperienceCard"
 import './ExperienceList.css'
 
 function ExperienceList() {
+
+  const [experiences, setExperiences] = useState(experienceData)
   return (
     <section>
       <h1>All experiences</h1>
       <div className='grid-view'>
-      {experiences.map((experience,index) => {
-        const { id, title, location,guests,availability,price } = experience;
-        return (
-          <Card key={index}>
-            <Image src='https://loremflickr.com/300/300' wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>{title}</Card.Header>
-              <Card.Meta>Price: {price}</Card.Meta>
-              <Card.Description>
-                <p>Location: {location}</p>
-                <p>Id: {id}</p>
-                <p>Guests: {guests}</p>
-                <p>Available? {availability ? 'Yes' : 'No' }</p>
-              </Card.Description>
-            </Card.Content>
-          </Card>
-        );
+      {experiences && experiences.map((experience,index) => {
+        return <ExperienceCard experience={experience} key={index} />
       })}
       </div>
     </section>
