c6b56f7df55c398772fc7f3d190ba543ee48b28d
Red <vhanchar@ucsc.edu>
2019-07-31 23:52:27 -0700

    got only 4 types of experiences to display

--- a-to-z/a-to-z/src/containers/LoggedIn/LoggedIn.js
+++ a-to-z/a-to-z/src/containers/LoggedIn/LoggedIn.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect}from 'react';
+import {Grid, Item} from 'semantic-ui-react';
 import axios from 'axios';
+import ExperienceTypeCard from './ExperienceTypeCard'
+
 
 const LoggedIn = () =>{
 
-    const [experience, setExperience] = useState();
-    console.log('before');
-
+    const [experience, setExperience] = useState([]);
+  
     useEffect(() => {
         const getExperiences = () => {
             // using mock data api since backend is not yet available
@@ -13,22 +15,33 @@ const LoggedIn = () =>{
             .get('https://my.api.mockaroo.com/atoz.json?key=73b335d0')
             .then(response => {
                 console.log(response.data);
+                setExperience((response.data));
             })
             .catch(error => {
                 console.log('unable to complete request');
             })
-
         }
         getExperiences();
-
-
     }, [])
 
-    console.log('after');
-
+    
     return(
-        <div> </div>
+
+        <div >
+            <Item>Chose Experience Type</Item>
+
+            <Grid columns={4} centered >
+            {experience.slice(0, 4).map((i, key) => (
+                <Grid.Column><ExperienceTypeCard key={key} title={i.titleActivity} image={i.imageActivity}/> </Grid.Column>
+            ))}
+
+            </Grid>
+
+
+        </div>
     )
+
+
 }
 
 export default LoggedIn;
